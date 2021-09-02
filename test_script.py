57d9fffc7279fb0161aa24c5a4bd9c3e942b4627
ErikBurdett <erikaburdet@gmail.com>
2021-09-02 12:42:17 -0500

    Removed Templates, added test_routes folder for testing

--- test_script.py
+++ test_script.py
@@ -1,6 +1,9 @@
 import unittest
 import requests
 import json
+import hello
+
+# At the moment all tests are manipulating existing data in the DB - this needs to be restructured to only test the functionality without manipulating data.
 
 
 class TestAPI(unittest.TestCase):
@@ -10,7 +13,8 @@ class TestAPI(unittest.TestCase):
         resp = requests.get(self.URL + '/get')
         self.assertEqual(resp.status_code, 200)
         # if delete test does not delete, this must be incremented
-        self.assertEqual(len(resp.json()), 9)
+        # self.assertEqual(resp.json())
+        print(resp.json())
         print("Test 1 completed, all articles expected recieved")
         # print(resp.json())
     def test_2_get_specific_article(self):
@@ -20,8 +24,8 @@ class TestAPI(unittest.TestCase):
         # print(resp.json())
         print("Test 2 Completed, retrieved specific article")
     def test_3_delete(self):
-        # for now, this id must be re-specified every time it's called
-        resp = requests.delete(self.URL + '/delete/' '/99')
+        # for now, this id must be re-specified every time it's called - best bet if Post is working to +1 to last id
+        resp = requests.delete(self.URL + '/delete/' '/105'  )
         self.assertEqual(resp.status_code, 200)
         # print(resp.json())
         print("Test 3 Completed, delete succesful")
@@ -35,11 +39,23 @@ class TestAPI(unittest.TestCase):
         print("test 4 completed successfully")
         print (resp.json())
         # if printed resp.json doesn't return a date or id something is broken
+        # from Jordan:
+        # after post is made, is the row present in the database ^ confirmed in the printed response above
+
+        # test test trying to create a test enviroment
+    # def test_1a_get_all(self):
+    #     tester = hello.test_client(self)
+    #     resp = requests.get(self.URL + '/get')
+    #     self.assertEqual(resp.status_code, 200)
+    #     # if delete test does not delete, this must be incremented
+    #     # self.assertEqual(len(resp.json()))
+    #     print(resp.json())
+    #     print("Test 1a completed, all articles expected recieved within a test environment")
+    #     # print(resp.json())
 
 
 
-# from Jordan:
-# after post is made, is the row present in the database ^ confirmed in the printed response above
+
 
 
 
