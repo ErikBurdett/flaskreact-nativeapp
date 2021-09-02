09040da9b8ce33cf566b2ce165e295ce936149e9
ErikBurdett <erikaburdet@gmail.com>
2021-09-01 21:00:05 -0500

    Created POST test - verifies writing ability to DB

--- test_script.py
+++ test_script.py
@@ -1,92 +1,48 @@
 import unittest
 import requests
+import json
 
 
 class TestAPI(unittest.TestCase):
     URL = 'https://flaskreact-native-test.herokuapp.com/'
-#     data =[{
-#     "body": "3030!!!",
-#     "date": "2021-08-27T15:41:44.032828",
-#     "id": 2,
-#     "title": "Deltron"
-#     },
-#     {
-#     "body": "Test 4",
-#     "date": "2021-08-27T17:35:58.872372",
-#     "id": 4,
-#     "title": "Test before deployment"
-#     },
-#     {
-#     "body": "Howdy! Edit test\n",
-#     "date": "2021-08-27T15:41:13.449348",
-#     "id": 1,
-#     "title": "DB deployed - 1st test - edit test"
-#     },
-#     {
-#     "body": "Howdy! ",
-#     "date": "2021-08-27T17:38:54.572090",
-#     "id": 5,
-#     "title": "Hello from the deployed application"
-#     }]
 
-#     expected_result_test3 ={
-#     "body": "3030!!!",
-#     "date": "2021-08-27T15:41:44.032828",
-#     "id": 2,
-#     "title": "Deltron"
-# }
     def test_1_get_all(self):
         resp = requests.get(self.URL + '/get')
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(len(resp.json()), 8)
-        print("Test 1 completed")
-        print(resp.json())
-        # if resp == self.data:
-        #     return print('data is correct')
-        # else:
-        #     return print('Something is different')
-    def test_2_get_all(self):
-        resp = requests.get(self.URL + '/get', json = self.data)
+        # if delete test does not delete, this must be incremented
+        self.assertEqual(len(resp.json()), 9)
+        print("Test 1 completed, all articles expected recieved")
+        # print(resp.json())
+    def test_2_get_specific_article(self):
+        # must be respecified if specific article is deleted
+        resp = requests.get(self.URL + '/get/' +'/66')
         self.assertEqual(resp.status_code, 200)
-        print(self.data)
-        print(resp.json())
-        # self.assertDictEqual(resp.dict(), self.data)
-        print("Test 2 Completed")
-    def test_3_get_specific_article(self):
-        resp = requests.get(self.URL + '/get/' +'/10')
+        # print(resp.json())
+        print("Test 2 Completed, retrieved specific article")
+    def test_3_delete(self):
+        # for now, this id must be re-specified every time it's called
+        resp = requests.delete(self.URL + '/delete/' '/98')
         self.assertEqual(resp.status_code, 200)
-        # self.assertDictEqual(resp.json(), self.expected_result_test3)
-        print(resp.json())
-        print("Test 3 Completed")
-    def test_4_delete(self):
-        resp = requests.delete(self.URL + '/delete/' '/14')
+        # print(resp.json())
+        print("Test 3 Completed, delete succesful")
+
+    #  successfuly posts a new article
+    def test_4_post(self):
+        # creates a test post, new incremented ID assigned
+        resp = requests.post(self.URL + '/add',
+        json={'title':'Testing POST Title', 'body':'Testing POST Body'})
         self.assertEqual(resp.status_code, 200)
-        print(resp.json())
-        print("Test 4 Completed")
-    # def test_5_post(self):
-    #     resp = requests.post(self.URL + '/post/'+'{new_post_obj}')
-    #     new_post_obj = {
-    #         "body": "DB Test POST request",
-    #         "date": "",
-    #         "id": 10,
-    #         "title": "DB POST TEST"
-    #     }
-# returning an error 405 !=200
-    # def test_5_update(self):
+        print("test 4 completed successfully")
+        print (resp.json())
+        # if printed resp.json doesn't return a date or id something is broken
 
 
 
+# from Jordan:
+# after post is made, is the row present in the database ^ confirmed in the printed response above
 
-    # # def test_get_articles(self):
-    # #     response = request.get(f'https://flaskreact-native-test.herokuapp.com/get')
-    # #     if response.ok:
-    # #         return response.text
-    # #     else:
-    # #         return 'Bad Response!'
-    # def test_get_articles(self):
-    #     with patch('articles.requests.get') as mocked_get:
-    #         mocked_get.return_value.ok = True
-    #         mocked_get.return_value.text = 'Success'
+
+
 
 
 
